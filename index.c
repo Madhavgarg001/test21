808e0175ec23c096551e550cd8f9d62f36179c81
Madhavgarg001 <sidharthgarg29082004@gmail.com>
2025-07-02 13:40:52 +0530

    intial commit

---
+++ index.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    // int age=19; 
+    // // printf("%d",age);
+    // // return 0;
+
+    // if(age>18){
+    //     printf("elder");
+    // }
+    // else{
+    //     printf("adult");
+
+    // }
+    // return 0;
+
+    int i; 
+    switch (i)
+    {
+    case 1:
+        printf("");
+        break;
+    
+    default:
+        break;
+    }
+
+    
+}
